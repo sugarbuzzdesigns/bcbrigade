aef44ffe101e0828556547905380a3d0626e831d
Patrick Hoydar <phoydar@webmd.net>
2015-06-15 09:33:38 -0400

    adding in video page

---
+++ themes/bones-bcbrigade/page-videos.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<div id="content">
+	<div id="inner-content" class="wrap cf">
+		<div class="entry-content entry">
+			<h1>Video Page</h1>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
