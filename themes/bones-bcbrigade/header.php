482986dec6de1d5c09765f7010a3b0ef2c585e98
Patrick Hoydar <phoydar@webmd.net>
2015-06-25 09:09:02 -0400

    removing uploads folder from git

--- themes/bones-bcbrigade/header.php
+++ themes/bones-bcbrigade/header.php
@@ -34,9 +34,16 @@
 		<?php wp_head(); ?>
 		<?php // end of wordpress head ?>
 
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+		  ga('create', 'UA-64453654-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
