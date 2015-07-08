32f43ec35ad6900d20b8e3dac5d7ecdf13270e9a
Patrick Hoydar <phoydar@webmd.net>
2015-07-08 15:37:46 -0400

    add more files

--- themes/bcbrigade/header-pre-release.php
+++ themes/bcbrigade/header-pre-release.php
@@ -22,22 +22,16 @@
 		} ?></title>
 		
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/app.css" />
-
-		<link rel="icon" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/favicon.ico" type="image/x-icon">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-114x114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-72x72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-precomposed.png">
 		
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-	<?php do_action('foundationPress_after_body'); ?>
+
 	
 	<div class="off-canvas-wrap" data-offcanvas>
 	<div class="inner-wrap">
 	
-	<?php do_action('foundationPress_layout_start'); ?>
+
 	
 	<nav class="tab-bar show-for-small-only">
 		<section class="left-small">
@@ -50,7 +44,5 @@
 		</section>
 	</nav>
 
-	<?php get_template_part('parts/off-canvas-menu'); ?>
 
 <section class="container" role="document">
-	<?php do_action('foundationPress_after_header'); ?>
